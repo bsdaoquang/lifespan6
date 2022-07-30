998938b9a7ef8eafe3880f014475cdefaab34a34
Đào Văn Quang <bsdaoquang@gmail.com>
2022-07-30 09:37:02 +0700

    lifespan6

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {View, Text} from 'react-native'
+
+const App = () => {
+  return(
+    <>
+      <Text>This is lifespan6</Text>
+    </>
+  )
+}
+
+export default App
